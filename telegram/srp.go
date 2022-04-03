96bd8c12c70200adfd63b43fc4f5344881a5e51b
Amarnath C <amarnathcharichilil@gmail.com>
2022-04-03 12:33:24 +0530

    Update srp.go

--- telegram/srp.go
+++ telegram/srp.go
@@ -1,23 +1,15 @@
-// Copyright (c) 2020-2021 KHS Films
-//
-// This file is a part of mtproto package.
-// See https://github.com/amarnathcjd/gogramblob/master/LICENSE for details
-
 package telegram
 
 import (
-	"github.com/pkg/errors"
-
+"fmt"
 	"github.com/amarnathcjd/gogram/telegram/internal/srp"
 )
 
 func GetInputCheckPassword(password string, accountPassword *AccountPassword) (InputCheckPasswordSRP, error) {
-	// У CurrentAlgo должен быть этот самый тип, с длинным названием алгоритма
-	// https://github.com/tdlib/td/blob/f9009cbc01e9c4c77d31120a61feb9c639c6aeda/td/telegram/AuthManager.cpp#L537
 	alg := accountPassword.CurrentAlgo
 	current, ok := alg.(*PasswordKdfAlgoSHA256SHA256PBKDF2HMACSHA512iter100000SHA256ModPow)
 	if !ok {
-		return nil, errors.New("invalid CurrentAlgo type")
+		return nil, fmt.Errorf("invalid CurrentAlgo type")
 	}
 
 	mp := &srp.ModPow{
@@ -29,7 +21,7 @@ func GetInputCheckPassword(password string, accountPassword *AccountPassword) (I
 
 	res, err := srp.GetInputCheckPassword(password, accountPassword.SRPB, mp)
 	if err != nil {
-		return nil, errors.Wrap(err, "processing password")
+		return nil, err
 	}
 
 	if res == nil {
