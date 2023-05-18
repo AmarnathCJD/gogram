597ce3c21ed648d9ca46bc2e2a62f775e72d10e3
RoseLoverX <112598515+RoseLoverX@users.noreply.github.com>
2023-05-18 20:28:29 +0530

    Update echobot.go
    reformat examples

--- examples/bot/echobot.go
+++ examples/bot/echobot.go
@@ -1,55 +1,57 @@
 package examples
 
 import (
-	"fmt"
-
 	"github.com/amarnathcjd/gogram/telegram"
 )
 
 const (
 	appID    = 6
-	appHash  = "YOUR_APP_HASH"
-	botToken = "YOUR_BOT_TOKEN"
+	appHash  = "app_hash"
+	botToken = "bot_token"
 )
 
 func main() {
 	// Create a new client
 	client, _ := telegram.NewClient(telegram.ClientConfig{
-		AppID:    appID,
-		AppHash:  appHash,
-		LogLevel: telegram.LogInfo,
+		AppID:   appID,
+		AppHash: appHash,
 	})
 
-	// Connect to the server
+	// Establish connection with the telegram server
 	if err := client.Connect(); err != nil {
 		panic(err)
 	}
 
 	// Authenticate the client using the bot token
-	if err := client.LoginBot(botToken); err != nil {
+	if err := client.Start(); err != nil {
 		panic(err)
 	}
 
-	// Add a message handler
-	client.AddMessageHandler(telegram.OnNewMessage, func(message *telegram.NewMessage) error {
-		var (
-			err error
-		)
-		// Print the message
-		fmt.Println(message.Marshal())
-
-		// Send a message
-		if message.IsPrivate() {
-			_, err = message.Respond(message)
-		}
-		return err
-	})
-
-	client.AddMessageHandler("/start", func(message *telegram.NewMessage) error {
-		message.Reply("Hello, I am a bot!")
+	// Add a message handler for echoing the messages back
+	client.AddMessageHandler(telegram.OnNewMessage, func(m *telegram.NewMessage) error {
+		m.Respond(m)
 		return nil
 	})
 
-	// Start polling
+	// Add a message handler for "/start" command
+	client.AddMessageHandler("/start", func(message *telegram.NewMessage) error {
+		var buttonBuilder = telegram.Button{}
+		message.Reply("Hello, I am a bot!", telegram.SendOptions{
+			ReplyMarkup: buttonBuilder.Keyboard(
+				buttonBuilder.Row(
+					buttonBuilder.URL(
+						"Gogram Repo", "github.com/amarnathcjd/gogram",
+					),
+				),
+			),
+			ParseMode:   telegram.HTML,
+			LinkPreview: true,
+			NoForwards:  true,
+		})
+
+		return nil
+	})
+
+	// Hold the current goroutine till client.Stop() is called.
 	client.Idle()
 }
