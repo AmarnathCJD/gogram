41bd14045be4bca0269f36efa99386c0da1a86eb
Emmanuel Ortiz <eos175@gmail.com>
2024-07-26 13:55:49 -0600

    Optimized string encoding by removing mod operations and reducing copies/assignments

--- internal/encoding/tl/cursor_w.go
+++ internal/encoding/tl/cursor_w.go
@@ -53,6 +53,11 @@ func (e *Encoder) PutBool(v bool) {
 	e.PutUint(uint32(crc))
 }
 
+func (e *Encoder) putUint8(v uint8) {
+	tmp := [1]byte{v}
+	e.write(tmp[:])
+}
+
 func (e *Encoder) PutUint(v uint32) {
 	buf := make([]byte, WordLen)
 	binary.LittleEndian.PutUint32(buf, v)
@@ -81,73 +86,38 @@ func (e *Encoder) PutDouble(v float64) {
 	e.write(buf)
 }
 
-func (e *Encoder) PutMessage(msg []byte) {
-	if len(msg) < MagicNumber {
-		e.putTinyBytes(msg)
-	} else {
-		e.putLargeBytes(msg)
+func (e *Encoder) PutMessage(b []byte) {
+	size := len(b)
+	pad := 0
+
+	maxLen := 1 << 24 // 3 bytes 24 bits, the first one is 0xfe, the remaining 3 times the length
+	if size > maxLen {
+		e.err = fmt.Errorf("message entity too large: expect less than %v, got %v", maxLen, size)
+		return
+	}
+
+	switch {
+	case size == 0: // optimizacion empty string
+		e.PutUint(0)
+		return
+	case size < MagicNumber:
+		pad = padding4(size + 1)
+		e.putUint8(uint8(size))
+	default:
+		pad = padding4(size)
+		e.PutUint(uint32(size)<<8 | MagicNumber)
+	}
+
+	e.write(b)
+
+	if pad > 0 {
+		var zero [4]byte
+		e.write(zero[:pad])
 	}
 }
 
 func (e *Encoder) PutString(msg string) {
 	e.PutMessage([]byte(msg))
-}
-
-func (e *Encoder) putTinyBytes(msg []byte) {
-	if len(msg) >= MagicNumber {
-		// it's panicing, cause, you shouldn' call this func by your
-		// hands. panic required for internal purposes
-		panic("tiny messages supports maximum 253 elements")
-	}
-
-	// Here we assume that the length of the resulting message should be divisible by 4
-	// (32/8 = 4, 4 bytes one word)
-	// so we create a buf with a size sufficient for storing
-	// array + 0-3 dot bytes that would have divided the result by 4
-	realBytesLen := 1 + len(msg) // adding 1, cause we need to store length, realBytesLen doesn't store
-	factBytesLen := realBytesLen
-	if factBytesLen%WordLen > 0 {
-		factBytesLen += WordLen - factBytesLen%WordLen
-	}
-
-	buf := make([]byte, factBytesLen)
-	buf[0] = byte(len(msg))
-	copy(buf[1:], msg)
-
-	e.write(buf)
-}
-
-func (e *Encoder) putLargeBytes(msg []byte) {
-	if len(msg) < MagicNumber {
-		// it's panicing, cause, you shouldn' call this func by your
-		// hands. panic required for internal purposes
-		panic("can't save binary stream with length less than 253 bytes")
-	}
-
-	maxLen := 1 << 24 // 3 bytes 24 bits, the first one is 0xfe, the remaining 3 times the length
-	if len(msg) > maxLen {
-		e.err = fmt.Errorf("message entity too large: expect less than %v, got %v", maxLen, len(msg))
-		return
-	}
-
-	realBytesLen := WordLen + len(msg) // First comes the magic byte and is 3 bytes long
-	factBytesLen := realBytesLen
-	if factBytesLen%WordLen > 0 {
-		factBytesLen += WordLen - factBytesLen%WordLen
-	}
-
-	// FIXME: this thing is uint number too. so, it can decode more simpler
-	littleEndianLength := make([]byte, 4)
-	binary.LittleEndian.PutUint32(littleEndianLength, uint32(len(msg)))
-
-	buf := make([]byte, factBytesLen)
-	buf[0] = byte(MagicNumber)
-	buf[1] = littleEndianLength[0]
-	buf[2] = littleEndianLength[1]
-	buf[3] = littleEndianLength[2]
-	copy(buf[WordLen:], msg)
-
-	e.write(buf)
 }
 
 func (e *Encoder) PutRawBytes(b []byte) {
