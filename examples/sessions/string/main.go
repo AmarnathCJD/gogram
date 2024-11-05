b1e1e37b4a4948d0e7b1d99afc498236ad186b6d
AmarnathCJD <amarnathcharichilil@gmail.com>
2024-11-05 17:29:06 +0530

    Add example to use `telethon` string session with gogram, also add string session generic example.

---
+++ examples/sessions/string/main.go
@@ -0,0 +1,36 @@
+package examples
+
+import (
+	"fmt"
+
+	"github.com/amarnathcjd/gogram/telegram"
+)
+
+func main() {
+	client, err := telegram.NewClient(telegram.ClientConfig{
+		AppID:   6,               // <App_ID>
+		AppHash: "YOUR_APP_HASH", // <App_Hash>
+		// StringSession: "<GOGRAM_STRING_SESSION>",
+		MemorySession: true,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	client.Conn()       // <Initiate the connection to Telegram Servers>
+	client.AuthPrompt() // <Auth Workflow>
+
+	// Print the StringSession
+	// This can be used to login without the need of reauthenticating again
+	// <Beware: This is sensitive information, do not share it with anyone>
+	// Doings so will allow Full Access to your Telegram Account
+	fmt.Println("StringSession:", client.ExportSession())
+
+	me, err := client.GetMe()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(client.JSON(me, true))
+}
