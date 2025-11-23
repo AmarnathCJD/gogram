a8a4cf9be46fdc6b3d6ccc61cae2c76de5a3271c
Amarnath <amarnathcharichilil@gmail.com>
2025-11-23 19:31:05 +0530

    replace %v with %w

--- mtproto.go
+++ mtproto.go
@@ -907,13 +907,13 @@ func (m *MTProto) startReadingResponses(ctx context.Context) {
 						m.Logger.Debug("tcp connection closed, reconnecting to [" + m.Addr + "] - <Tcp> ...")
 						err = m.Reconnect(false)
 						if err != nil {
-							m.Logger.Error("failed to reconnect to [%s] - <%s>: %w", utils.FmtIp(m.Addr), m.GetTransportType(), err)
+							m.Logger.Error("failed to reconnect to [%s] - <%s>: %v", utils.FmtIp(m.Addr), m.GetTransportType(), err)
 						}
 					} else if strings.Contains(err.Error(), "required to reconnect!") { // network is not stable
 						m.Logger.Debug("packet read error, reconnecting to [" + m.Addr + "] - <Tcp> ...")
 						err = m.Reconnect(false)
 						if err != nil {
-							m.Logger.Error("failed to reconnect to [%s] - <%s>: %w", utils.FmtIp(m.Addr), m.GetTransportType(), err)
+							m.Logger.Error("failed to reconnect to [%s] - <%s>: %v", utils.FmtIp(m.Addr), m.GetTransportType(), err)
 						}
 					}
 				}
@@ -926,7 +926,7 @@ func (m *MTProto) startReadingResponses(ctx context.Context) {
 					m.Logger.Debug("eof error, reconnecting to [" + m.Addr + "] - <Tcp> ...")
 					err = m.Reconnect(false)
 					if err != nil {
-						m.Logger.Error("failed to reconnect to [%s] - <%s>: %w", utils.FmtIp(m.Addr), m.GetTransportType(), err)
+						m.Logger.Error("failed to reconnect to [%s] - <%s>: %v", utils.FmtIp(m.Addr), m.GetTransportType(), err)
 					}
 					return
 				default:
@@ -968,7 +968,7 @@ func (m *MTProto) handle404Error() {
 		m.Logger.Debug("-404 error occurred %d times, attempting to reconnect", m.authKey404[0])
 		err := m.Reconnect(false)
 		if err != nil {
-			m.Logger.Error("failed to reconnect to [%s] - <%s>: %w", utils.FmtIp(m.Addr), m.GetTransportType(), err)
+			m.Logger.Error("failed to reconnect to [%s] - <%s>: %v", utils.FmtIp(m.Addr), m.GetTransportType(), err)
 		}
 	} else if m.authKey404[0] >= 16 {
 		panic("[AUTH_KEY_INVALID] (code -404) - too many failures")
