37f5d269374ec9d8ebc243d2cd0b8f49002614e0
Amarnath <amarnathcharichilil@gmail.com>
2025-12-12 09:45:24 +0530

    bug fixes, proper logging for debug.

--- internal/transport/writer.go
+++ internal/transport/writer.go
@@ -2,32 +2,21 @@ package transport
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"io"
 	"net"
 	"sync"
 	"time"
 )
 
-var ErrClosed = errors.New("reader closed")
-
 type Reader struct {
 	r      io.Reader
 	ctx    context.Context
 	cancel context.CancelFunc
 
-	req  chan []byte
-	done chan readResult
-
-	mu     sync.Mutex
-	closed bool
-
-	wg sync.WaitGroup
-}
-
-type readResult struct {
-	n   int
-	err error
+	mu            sync.Mutex
+	closed        bool
+	interruptOnce sync.Once
 }
 
 func NewReader(ctx context.Context, r io.Reader) *Reader {
@@ -37,86 +26,51 @@ func NewReader(ctx context.Context, r io.Reader) *Reader {
 		r:      r,
 		ctx:    ctx,
 		cancel: cancel,
-		req:    make(chan []byte, 4),
-		done:   make(chan readResult, 1),
 	}
 
-	c.wg.Add(1)
-	go c.worker()
+	go func() {
+		<-c.ctx.Done()
+		c.interrupt()
+	}()
 
 	return c
 }
 
-func (c *Reader) worker() {
-	defer c.wg.Done()
-
-	for {
-		select {
-		case <-c.ctx.Done():
-			return
-
-		case buf, ok := <-c.req:
-			if !ok {
-				return
-			}
-
-			n, err := c.read(buf)
-
-			select {
-			case c.done <- readResult{n, err}:
-			case <-c.ctx.Done():
-				return
-			}
-		}
-	}
-}
-
 func (c *Reader) interrupt() {
-	if nc, ok := c.r.(net.Conn); ok {
-		nc.SetReadDeadline(time.Now())
-		nc.Close()
-	} else if closer, ok := c.r.(io.Closer); ok {
-		closer.Close()
-	}
-}
-
-func (c *Reader) read(buf []byte) (int, error) {
-	n := 0
-	for n < len(buf) {
-		select {
-		case <-c.ctx.Done():
-			return n, c.ctx.Err()
-		default:
-			k, err := c.r.Read(buf[n:])
-			n += k
-			if err != nil {
-				return n, err
-			}
+	c.interruptOnce.Do(func() {
+		if nc, ok := c.r.(net.Conn); ok {
+			nc.SetReadDeadline(time.Now())
+			nc.Close()
+		} else if closer, ok := c.r.(io.Closer); ok {
+			closer.Close()
 		}
-	}
-	return n, nil
+	})
 }
 
 func (c *Reader) Read(p []byte) (int, error) {
 	c.mu.Lock()
 	if c.closed {
 		c.mu.Unlock()
-		return 0, ErrClosed
+		return 0, fmt.Errorf("reader is closed")
+	}
+	r := c.r
+	ctx := c.ctx
+	c.mu.Unlock()
+
+	if err := ctx.Err(); err != nil {
+		return 0, err
+	}
+
+	n, err := io.ReadFull(r, p)
+	if err != nil {
+		return n, err
 	}
 
 	select {
-	case <-c.ctx.Done():
-		c.mu.Unlock()
-		return 0, c.ctx.Err()
-	case c.req <- p:
-		c.mu.Unlock()
-	}
-
-	select {
-	case <-c.ctx.Done():
-		return 0, c.ctx.Err()
-	case res := <-c.done:
-		return res.n, res.err
+	case <-ctx.Done():
+		return n, ctx.Err()
+	default:
+		return n, nil
 	}
 }
 
@@ -129,9 +83,7 @@ func (c *Reader) Close() error {
 	c.closed = true
 	c.mu.Unlock()
 
-	c.interrupt()
 	c.cancel()
-	close(c.req)
-	c.wg.Wait()
+	c.interrupt()
 	return nil
 }
