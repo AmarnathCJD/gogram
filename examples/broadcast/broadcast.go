f6ef422fcb5a98c136611e03f5a7d11103d8c61a
AmarnathCJD <amarnathcharichilil@gmail.com>
2026-02-02 07:27:19 +0530

    add broadcast example for bots

---
+++ examples/broadcast/broadcast.go
@@ -0,0 +1,40 @@
+package examples
+
+import (
+	"github.com/amarnathcjd/gogram/telegram"
+)
+
+// Broadcasting to bot users / chats using updates.GetDifference
+// client.Broadcast returns peers in update history via channels
+// these peers can be used for broadcasting messages
+// no external database is required to store user / chat ids
+
+const (
+	appID    = 6
+	appHash  = "YOUR_APP_HASH"
+	botToken = "YOUR_BOT_TOKEN"
+)
+
+func main() {
+	// create a new client object
+	client, _ := telegram.NewClient(telegram.ClientConfig{
+		AppID:    appID,
+		AppHash:  appHash,
+		LogLevel: telegram.LogInfo,
+	})
+
+	client.LoginBot(botToken)
+
+	users, chats, err := client.Broadcast()
+	if err != nil {
+		panic(err)
+	}
+
+	for user := range users {
+		client.SendMessage(user, "Hello, This is a broadcast message")
+	}
+
+	for chat := range chats {
+		client.SendMessage(chat, "Hello, This is a broadcast message")
+	}
+}
