6958d92c9c5e28c8adc5368c23218d84b2eaf12e
AmarnathCJD <72609355+AmarnathCJD+@users.noreply.github.com>
2026-02-02 07:27:19 +0530

    tlgen: update TL schema files

--- README.md
+++ README.md
@@ -100,7 +100,7 @@ If you'd like to support Gogram, you can consider:
   <li><strong>up-to-date</strong>: 🔄 gogram is always in sync with the latest telegram api changes and additions (<code>tl-parser</code> is used to generate the api layer).</li>
 </ul>
 
-#### Current Layer - **187** (Updated on 2024-09-18)
+#### Current Layer - **188** (Updated on 2024-09-19)
 
 ## doing stuff
 
--- telegram/types_gen.go
+++ telegram/types_gen.go
@@ -221,15 +221,6 @@ func (*AccountDaysTtl) CRC() uint32 {
 	return 0xb8d0afdf
 }
 
-// Contains the link that must be used to open a [direct link Mini App](https://core.telegram.org/api/bots/webapps#direct-link-mini-apps).
-type AppWebViewResultURL struct {
-	URL string
-}
-
-func (*AppWebViewResultURL) CRC() uint32 {
-	return 0x3c1b4f0d
-}
-
 // Represents a [bot mini app that can be launched from the attachment/side menu »](https://core.telegram.org/api/bots/attach)
 type AttachMenuBot struct {
 	Inactive                 bool `tl:"flag:0,encoded_in_bitflags"`
@@ -1217,18 +1208,13 @@ func (*Error) CRC() uint32 {
 
 // Exported [chat folder deep link »](https://core.telegram.org/api/links#chat-folder-links).
 type ExportedChatlistInvite struct {
-	Revoked bool `tl:"flag:0,encoded_in_bitflags"`
-	Title   string
-	URL     string
-	Peers   []Peer
+	Title string
+	URL   string
+	Peers []Peer
 }
 
 func (*ExportedChatlistInvite) CRC() uint32 {
 	return 0xc5181ac
-}
-
-func (*ExportedChatlistInvite) FlagIndex() int {
-	return 0
 }
 
 // Describes a [temporary profile link](https://core.telegram.org/api/links#temporary-profile-links).
@@ -1285,17 +1271,6 @@ type FileHash struct {
 
 func (*FileHash) CRC() uint32 {
 	return 0xf39b035c
-}
-
-// File is currently unavailable.
-type FileLocationUnavailable struct {
-	VolumeID int64
-	LocalID  int32
-	Secret   int64
-}
-
-func (*FileLocationUnavailable) CRC() uint32 {
-	return 0x7c596b46
 }
 
 // Folder
@@ -3506,15 +3481,6 @@ func (*ShippingOption) CRC() uint32 {
 	return 0xb6213cdf
 }
 
-// Contains the webview URL with appropriate theme parameters added
-type SimpleWebViewResultURL struct {
-	URL string
-}
-
-func (*SimpleWebViewResultURL) CRC() uint32 {
-	return 0x882f76bb
-}
-
 // Info about an SMS job.
 type SmsJob struct {
 	JobID       string
@@ -3710,7 +3676,6 @@ type StarsTransaction struct {
 	Failed             bool `tl:"flag:6,encoded_in_bitflags"`
 	Gift               bool `tl:"flag:10,encoded_in_bitflags"`
 	Reaction           bool `tl:"flag:11,encoded_in_bitflags"`
-	Subscription       bool `tl:"flag:12,encoded_in_bitflags"`
 	ID                 string
 	Stars              int64
 	Date               int32
